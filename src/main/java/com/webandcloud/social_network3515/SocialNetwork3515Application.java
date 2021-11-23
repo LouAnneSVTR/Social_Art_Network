843648e6853fcd0938c4a5b2f3ead48d2a5bdb09
e181808s <sofiane.couedel@etu.univ-nantes.fr>
2021-11-23 17:18:36 +0100

    0Auth 2 google cloud

--- src/main/java/com/webandcloud/social_network3515/SocialNetwork3515Application.java
+++ src/main/java/com/webandcloud/social_network3515/SocialNetwork3515Application.java
@@ -2,8 +2,10 @@ package com.webandcloud.social_network3515;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SocialNetwork3515Application {
 
     public static void main(String[] args) {

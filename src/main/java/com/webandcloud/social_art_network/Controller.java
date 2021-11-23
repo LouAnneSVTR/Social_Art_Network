5cff72c7754809085839e209082c4dcd77ae63e0
e181808s <sofiane.couedel@etu.univ-nantes.fr>
2021-11-23 21:23:46 +0100

    test

--- src/main/java/com/webandcloud/social_art_network/Controller.java
+++ src/main/java/com/webandcloud/social_art_network/Controller.java
@@ -9,7 +9,7 @@ public class Controller {
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+        return String.format("Hello! %s!", name);
     }
 
 

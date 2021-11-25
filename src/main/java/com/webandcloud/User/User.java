d279f17627dc533daa7d401254584a4f30acda1e
E185574L <lou-anne.sauvetre@etu.univ-nantes.fr>
2021-11-25 15:36:41 +0100

    Add Users update with JCP datastore

--- src/main/java/com/webandcloud/User/User.java
+++ src/main/java/com/webandcloud/User/User.java
@@ -1,19 +1,67 @@
 package com.webandcloud.User;
 
-import javax.persistence.*;
+import com.google.cloud.Date;
+import com.google.cloud.spring.data.datastore.core.mapping.Entity;
+import org.springframework.data.annotation.Id;
+
 
 @Entity
-@Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
 
-    @Column(name = "name")
-    private String userName;
+    private String userFirstName;
 
-    @Column(name = "email")
+    private String userLastName;
+
     private String userEmail;
+
+    private Date dateOfSubcribtion;
+
+    public User(Integer userId, String userFirstName, String userLastName, String userEmail) {
+        this.userId = userId;
+        this.userFirstName = userFirstName;
+        this.userLastName = userLastName;
+        this.userEmail = userEmail;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + this.userId +
+                ", first name='" + this.userFirstName + '\'' +
+                ", last name ='" + this.userLastName + '\'' +
+                ", email=" + this.userEmail +
+                '}';
+    }
 
 }
--- src/main/java/com/webandcloud/User/UserRepository.java
+++ src/main/java/com/webandcloud/User/UserRepository.java
@@ -1,17 +1,14 @@
 package com.webandcloud.User;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+import com.google.cloud.spring.data.datastore.repository.DatastoreRepository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends DatastoreRepository<User, Long> {
 
     List<User> findAll();
 
-    Optional<User> findByEmail(String email);
+    //Optional<User> findByEmail(String email);
 
     User findUserById(Integer id);
 

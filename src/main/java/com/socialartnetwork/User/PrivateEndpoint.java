3bc59b5db05a79e72ee0a40a4d26f51b12a4de64
e181808s <sofiane.couedel@etu.univ-nantes.fr>
2021-11-28 18:57:24 +0100

    update

--- src/main/java/com/socialartnetwork/User/PrivateEndpoint.java
+++ src/main/java/com/socialartnetwork/User/PrivateEndpoint.java
@@ -4,7 +4,7 @@ import com.socialartnetwork.User.dto.UserDto;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+//import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,13 +31,13 @@ public class PrivateEndpoint {
     //SecurityService securityService;
 
     @GetMapping("user-details")
-    public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal UserDto userDto) {
+    public ResponseEntity<?> getUserInfo(/*@AuthenticationPrincipal*/ UserDto userDto) {
         User user = userService.getUser(userDto.getEmail());
         return ResponseEntity.ok(user);
     }
 
     @GetMapping("saveUser")
-    public ResponseEntity<UserDto> saveUserInfo(@AuthenticationPrincipal UserDto userDto) {
+    public ResponseEntity<UserDto> saveUserInfo(/*@AuthenticationPrincipal*/ UserDto userDto) {
         userService.saveUser(userDto);
         return ResponseEntity.ok(userDto);
     }

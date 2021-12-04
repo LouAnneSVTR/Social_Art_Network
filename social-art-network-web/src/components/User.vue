ae7827e300f09c96cedd14d24ec7c4cedc9730e3
e181808s <sofiane.couedel@etu.univ-nantes.fr>
2021-12-04 11:02:06 +0100

    Display User data in Vue3

---
+++ social-art-network-web/src/components/User.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class = "container">
+    <h1 class = "text-center">Users List</h1>
+    <table class = "table table-striped">
+      <thead>
+        <th> User Id        </th>
+        <th> User firstname </th>
+        <th> User lastname  </th>
+        <th> User email     </th>
+      </thead>
+      <tbody>
+        <tr v-for = "user in users" v-bind:key = "user.id">
+          <td> {{user.id}} </td>
+          <td> {{user.userLastName}} </td>
+          <td> {{user.userLastName}} </td>
+          <td> {{user.email}} </td>
+          
+        </tr>
+      </tbody>
+
+    </table>
+  </div>
+</template>
+
+<script>
+import UserService from "../services/UserService";
+  export default {
+    name: 'Users',
+    data(){
+      return {
+        users: []
+      }
+
+    },
+    methods: {
+      getUsers(){
+        UserService.getUsers().then((response) => {
+          this.users = response.data;
+        });
+      }
+    },
+    created() {
+      this.getUsers()
+    }
+  }
+</script>
+
+<style>
+
+</style>
